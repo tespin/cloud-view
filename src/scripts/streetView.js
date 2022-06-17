57c638aecfca711b9117300aa2b8723f732c1539
Tristan Espinoza <tristan@Tristans-MacBook-Pro.local>
2022-06-16 22:16:05 -0700

    changing image size by parent width

--- src/scripts/streetView.js
+++ src/scripts/streetView.js
@@ -1,7 +1,7 @@
 
 let lat, lon;
 let api = "https://maps.googleapis.com/maps/api/streetview?";
-let size = "size=400x400";
+let size = "size=";
 let fov = "&fov=40";
 let heading = "&heading=";
 let pitch = "&pitch=90";
@@ -51,6 +51,8 @@ document.getElementById('geolocate').addEventListener('click', event => {
 });
 
 function getUrl() {
+    let parentDiv = document.getElementById("imageContainer");
+    size += `${parentDiv.offsetWidth}x${parentDiv.offsetWidth}`;
     let loc = `&location=${lat},${lon}`;
     let url = api + size + loc + fov + heading + pitch + apiKey;
     return url;
