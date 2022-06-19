03ce43df3e33c31870ba3a26fddb73fb039c3a21
Tristan Espinoza <tristan@Tristans-MacBook-Pro.local>
2022-06-19 13:11:40 -0700

    cleaning up console logs

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ app.listen(port, () => console.log(`listening at port ${port}`));
 app.use(express.static('src'));
 app.use(express.json({limit: '1mb'}));
 app.post('/api', (request, response) => {
-    console.log('I got a request!');
-    console.log(request.body);
     const data = request.body;
     response.json({
         status: 'success',
