7613becbd393a1894ad9ac912c2ec11f1fd87bbb
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-22 08:03:24 -0700

    changing file paths to get index page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`listening at port ${port}`));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('/public'));
 app.use(express.json({limit: '1mb'}));
 app.post('/api', (request, response) => {
     const data = request.body;
