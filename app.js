98d5dfae819bd5e5b45b08ac5f9730f528ceb600
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-22 08:43:28 -0700

    moving above express static

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`listening at port ${port}`));
-app.route('/').get((req, res) => {
-    res.render('/first.html');
-});
 app.use(express.static('public'));
 // app.set('views', path.join(__dirname, 'views'));
 app.use(express.json({limit: '1mb'}));
-// app.route('/').get((req, res) => {
-//     res.render('/first.html');
-// });
+app.route('/').get((req, res) => {
+    res.render('/first.html');
+});
 app.post('/api', (request, response) => {
     const data = request.body;
     response.json({
