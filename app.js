0819c5abaa1680bad099e2f76cd770df4a7a1f29
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-22 08:07:55 -0700

    attempting to serve index page

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`listening at port ${port}`));
-app.use(express.static('/public'));
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 app.post('/api', (request, response) => {
     const data = request.body;
