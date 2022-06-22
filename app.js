a29dcc410eeec31697ce506388458d052247af39
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-22 08:37:18 -0700

    attempting to get html page in views folder

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`listening at port ${port}`));
-// app.use(express.static('public'));
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+// app.set('views', path.join(__dirname, 'views'));
 app.use(express.json({limit: '1mb'}));
 app.route('/').get((req, res) => {
     res.render('/first.html');
