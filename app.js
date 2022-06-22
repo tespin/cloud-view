9fa52bb956a09b495f8a17357c34f1f433090cae
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-22 08:33:37 -0700

    not using static files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`listening at port ${port}`));
-app.use(express.static('public'));
+// app.use(express.static('public'));
 // app.set('views', path.join(__dirname, 'views'));
 app.use(express.json({limit: '1mb'}));
 app.route('/').get((req, res) => {
