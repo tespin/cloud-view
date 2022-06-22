1f7fc061b224bdb36bd5448c5793a5f60c950e5a
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-22 08:47:29 -0700

    attempting to get html page in views folder

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 // app.set('views', path.join(__dirname, 'views'));
 app.use(express.json({limit: '1mb'}));
 app.route('/').get((req, res) => {
-    res.render('/views/first.html');
+    res.render('views/first.html');
 });
 app.post('/api', (request, response) => {
     const data = request.body;
