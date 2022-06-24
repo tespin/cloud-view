2a6974267c2ce27aa535cd728cd3240d99db5da0
Tristan Espinoza <tristan@Tristans-MacBook-Pro.local>
2022-06-24 00:14:00 -0700

    adding sqlite

--- app.js
+++ app.js
@@ -2,32 +2,22 @@ const express = require('express');
 const path = require('path');
 const passport = require('passport');
 const session = require('express-session');
+const SQLiteStore = require('better-sqlite3-session-store')(session);
 const authRouter = require('./routes/auth');
 const app = express();
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`listening at port ${port}`));
-// app.use('/', authRouter);
-app.use(express.static('public'));
-app.use(express.json({limit: '1mb'}));
 app.use('/', authRouter);
-// app.use(express.static('views'));
-// app.get('/', (req, res) => {
-    // res.render('./views/first.html');
-    // res.sendFile('first.html');
-//     res.sendFile('./public/views/first.html');    
-// });
-// app.route('/').get((req, res) => {
-//     res.sendFile(path.join(__dirname + '/views/first.html'));
-//     // console.log('not found');
-//     // res.sendFile('/views/firs t.html');
-// });
-// app.use(express.static(__dirname, '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: 'sessions.db', dir: './var/db'})
+}));
+app.use(passport.authenticate('session'));
+app.use(express.json({limit: '1mb'}));
 
-// app.set('views', path.join(__dirname, 'views'));
-// app.route('/').get((req, res) => {
-//     console.log(path.join(__dirname + '/views/index.html'));
-//     res.sendFile(path.join(__dirname + '/views/index.html'));
-// });
 app.post('/api', (request, response) => {
     const data = request.body;
     response.json({
--- routes/auth.js
+++ routes/auth.js
@@ -2,19 +2,62 @@ const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 let crypto = require('crypto');
+let db = require('../db');
 const router = express.Router();
 
-router.get('/signup.html', (req, res) => {
-    console.log('on login page');
-    // res.sendFile('/signup.html');
-    // res.send('on sign up page');
-    // console.log('on sign up page');
-    // res.sendFile('./signup.html');
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+    db.prepare('SELECT * FROM users WHERE username = ?').get([username]), (err, row) => {
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false, { message: 'Incorrect username or password.'}); }
+        
+        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', (err, hashedPassword) => {
+            if (err) { return cb(err); }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+                return cb(null, false, { message: 'Incorrect username or password.'});
+            }
+
+            return cb(null, row);
+        });
+    };
+}));
+    // }));
+
+    //     crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', (err, hashedPassword {
+    //         if (err) { return cb(err); }
+    //         if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+    //             return cb(null, false, {message: 'Incorrect username or password.'});
+    //         });
+    //     });
+    // }));
+
+passport.serializeUser((user, cb) => {
+    process.nextTick( () => {
+        cb(null, { id: user.id, username: user.username });
+    });
 });
 
-router.get('/test', (req, res) => {
-    console.log('test log');
-    res.send('on test page');
-})
+passport.deserializeUser( (user, cb) => {
+    process.nextTick( () => {
+        return cb(null, user);
+    });
+});
+
+
+router.post('/login/password', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login.html'
+}));
+// router.get('/signup.html', (req, res) => {
+//     console.log('on login page');
+//     // res.sendFile('/signup.html');
+//     // res.send('on sign up page');
+//     // console.log('on sign up page');
+//     // res.sendFile('./signup.html');
+// });
+
+// router.get('/test', (req, res) => {
+//     console.log('test log');
+//     res.send('on test page');
+// })
 
 module.exports = router;
