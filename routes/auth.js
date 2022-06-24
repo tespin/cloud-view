120970974ccf49db41aa6d8b2549dece259f1e44
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-24 12:41:50 -0700

    adding sqlite db

--- routes/auth.js
+++ routes/auth.js
@@ -6,20 +6,36 @@ let db = require('../db');
 const router = express.Router();
 
 passport.use(new LocalStrategy(function verify(username, password, cb) {
-    db.prepare('SELECT * FROM users WHERE username = ?').get([username]), (err, row) => {
+    db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
         if (err) { return cb(err); }
         if (!row) { return cb(null, false, { message: 'Incorrect username or password.'}); }
-        
-        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', (err, hashedPassword) => {
+
+        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
             if (err) { return cb(err); }
-            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
-                return cb(null, false, { message: 'Incorrect username or password.'});
+            if (!crypto.timingSafeEqual(row.hashedPassword, hashedPassword)) {
+                return cb(null, false, { message: 'Incorrect username or password'});
             }
 
             return cb(null, row);
         });
-    };
+    });
 }));
+
+// passport.use(new LocalStrategy(function verify(username, password, cb) {
+//     db.prepare('SELECT * FROM users WHERE username = ?').get([username]), (err, row) => {
+//         if (err) { return cb(err); }
+//         if (!row) { return cb(null, false, { message: 'Incorrect username or password.'}); }
+        
+//         crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', (err, hashedPassword) => {
+//             if (err) { return cb(err); }
+//             if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+//                 return cb(null, false, { message: 'Incorrect username or password.'});
+//             }
+
+//             return cb(null, row);
+//         });
+//     };
+// }));
     // }));
 
     //     crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', (err, hashedPassword {
