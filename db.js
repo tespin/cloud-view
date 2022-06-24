96422c171aae93821bcdffd7c9ee4fd800d63261
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-24 12:17:40 -0700

    not using better-sqlite3 for now

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-const sqlite3 = require('better-sqlite3');
+const sqlite3 = require('sqlite3');
 const mkdirp = require('mkdirp');
 const crypto = require('crypto');
 
 mkdirp.sync('./var/db');
 
-const db = new sqlite3('./var/db/gallery.db', { verbose: console.log});
+const db = new sqlite3.Database('./var/db/gallery.db');
 
 db.serialize( () => {
-    db.prepare("CREATE TABLE IF NOT EXISTS users ( \
+    db.run("CREATE TABLE IF NOT EXISTS users ( \
         id INTEGER PRIMARY KEY, \
         username TEXT UNIQUE, \
         hashed_password BLOB, \
@@ -15,29 +15,29 @@ db.serialize( () => {
         name TEXT, \
         email TEXT UNIQUE, \
         email_verified INTEGER \ )"
-    ).run();
+    );
     
-    db.prepare("CREATE TABLE IF NOT EXISTS federated_credentials ( \
+    db.run("CREATE TABLE IF NOT EXISTS federated_credentials ( \
         id INTEGER PRIMARY KEY, \
         user_id INTEGER NOT NULL, \
         provider TEXT NOT NULL, \
         subject TEXT NOT NULL, \
         UNIQUE (provider, subject) \ )"
-    ).run();
+    );
     
-    db.prepare("CREATE TABLE IF NOT EXISTS saved ( \
+    db.run("CREATE TABLE IF NOT EXISTS saved ( \
         id INTEGER PRIMARY KEY, \
         owner_id INTEGER NOT NULL, \
         date TEXT NOT NULL, \
         image BLOB \ )"
-    ).run();
+    );
 
     let salt = crypto.randomBytes(16);
-    db.prepare("INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)", [
+    db.run("INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)", [
         'testAccount',
         crypto.pbkdf2Sync('password', salt, 310000, 32, 'sha256'),
         salt
-    ]).run();
+    ]);
 });
 
 module.exports = db;
