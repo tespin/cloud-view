cf648fca5cd53794ddb5f4c9111f32a4b383710f
Tristan Espinoza <tristan@Tristans-MacBook-Pro.local>
2022-06-26 01:13:45 -0700

    creating endpoint for save function

--- public/scripts/streetView.js
+++ public/scripts/streetView.js
@@ -49,13 +49,20 @@ document.getElementById('geolocate').addEventListener('click', event => {
     }
 });
 
-document.getElementById('save').addEventListener('click', event => {
-    // if image is not undefined
+// document.getElementById('save').addEventListener('click', event => {
+//     // if image is not undefined
 
-    let img = document.getElementById("result");
-    let base64 = getBase64Image(img);
-    const data = { base64 };
-});
+//     let img = document.getElementById("result");
+//     let base64 = getBase64Image(img);
+//     const data = { base64 };
+
+//     const options = {
+//         method: 'POST', headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     };
+// });
 
 function getUrl() {
     let parentDiv = document.getElementsByClassName("container")[0];
@@ -65,14 +72,14 @@ function getUrl() {
     return url;
 }
 
-function getBase64Image(img) {
-    let canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    let ctx = canvas.getContext("2d");
-    let.drawImage(img, 0, 0);
-    let dataURL = canvas.toDataURL("image/png");
-    // return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-    return dataURL;
-  }
+// function getBase64Image(img) {
+//     let canvas = document.createElement("canvas");
+//     canvas.width = img.width;
+//     canvas.height = img.height;
+//     let ctx = canvas.getContext("2d");
+//     let.drawImage(img, 0, 0);
+//     let dataURL = canvas.toDataURL("image/png");
+//     // return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+//     return dataURL;
+//   }
   
