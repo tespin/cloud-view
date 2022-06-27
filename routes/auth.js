f514614875e61e76e5765c26ed9a696f70eb06c2
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-27 16:57:44 -0700

    adding authentication

--- routes/auth.js
+++ routes/auth.js
@@ -23,31 +23,30 @@ router.post('/signup', function(req, res, next) {
             console.log(`There was an error signing up: ${err}`);
             return next(err);
         }
-
-        const authenticate = User.authenticate();
-        authenticate('username', 'password', function(err, result) {
-            if (err) {
-                console.log(`There was an error authenticating: ${err}`);
-                return next(err);
-            }
-        });
-
-        console.log('registration successful');
-        res.redirect('/profile.html');
+    });
+}, passport.authenticate('local', { failureRedirect: '/signup.html'}),
+        function(req, res, next) {
+            console.log('registration successful');
+            res.redirect('/profile.html');
+        }
+);
 
         // const authenticate = User.authenticate();
         // authenticate('username', 'password', function(err, result) {
         //     if (err) {
-        //         console.log
+        //         console.log(`There was an error authenticating: ${err}`);
+        //         return next(err);
         //     }
         // });
+
+        // console.log('registration successful');
+        // res.redirect('/profile.html');
+
         // passport.authenticate('local', { failureRedirect: '/signup.html'}),
         //     function(req, res, next) {
         //         console.log('registration successful');
         //         res.redirect('/profile.html');
         //     }
-    });
-});
 
 router.post('/login/password', passport.authenticate('local', {
     successRedirect: '/profile.html',
@@ -127,28 +126,28 @@ function ensureAuthenticated(req, res, next) {
 //         });
 //     });
 
-//     router.post('/signup', function(req, res, next) {
-//         // console.log(`attempting signup ${req.body.username}`);
-//         myDataBase.findOne({ username: req.body.username}, function(err, user) {
-//             if (err) { next(err); }
-//             else if (user) { res.redirect('/profile.html'); }
-//             else {
-//                 const hash = bcrypt.hashSync(req.body.password, 12);
-//                 myDataBase.insertOne ({
-//                     username: req.body.username,
-//                     password: hash
-//                 }, function(err, doc) {
-//                         if (err) { return next(err); }
-//                         else { res.redirect('/profile.html'); }
-//                     }
-//                 )
-//             }
-//         })
-//     },  passport.authenticate('local', { failureRedirect: '/signup.html'}),
-//             function(req, res, next) {
-//                 res.redirect('/profile.html');
-//             }
-//     );
+    // router.post('/signup', function(req, res, next) {
+    //     // console.log(`attempting signup ${req.body.username}`);
+    //     myDataBase.findOne({ username: req.body.username}, function(err, user) {
+    //         if (err) { next(err); }
+    //         else if (user) { res.redirect('/profile.html'); }
+    //         else {
+    //             const hash = bcrypt.hashSync(req.body.password, 12);
+    //             myDataBase.insertOne ({
+    //                 username: req.body.username,
+    //                 password: hash
+    //             }, function(err, doc) {
+    //                     if (err) { return next(err); }
+    //                     else { res.redirect('/profile.html'); }
+    //                 }
+    //             )
+    //         }
+    //     })
+    // },  passport.authenticate('local', { failureRedirect: '/signup.html'}),
+    //         function(req, res, next) {
+    //             res.redirect('/profile.html');
+    //         }
+    // );
     
 //     router.post('/login/password', passport.authenticate('local', {
 //         successRedirect: '/profile.html',
