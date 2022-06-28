fddf167bba0e476eef1e71ce40dbcd27d7f37559
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-27 19:19:09 -0700

    test

--- routes/auth.js
+++ routes/auth.js
@@ -15,8 +15,6 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-router.use('/profile.html', ensureAuthenticated);
-
 router.post('/signup', function(req, res) {
     console.log(`attempting to register user: ${req.body.username}`);
     User.register(new User({ username: req.body.username }), req.body.password, function (err, user) {
@@ -24,50 +22,26 @@ router.post('/signup', function(req, res) {
             console.log(`There was an error signing up: ${err}`);
             return res.redirect('/signup.html');
         }
-        // const authenticate = User.authenticate();
-        // authenticate('username', 'password', function(err, result) {
-        //     if (err) {
-        //         console.log(`There was an error authenticating: ${err}`);
-        //         return next(err);
-        //     }
+        console.log('beginning authentication');
 
-        //     console.log('authentication successful ...?');
-        //     res.redirect('/profile.html');
-        // });
+        const authenticate = User.authenticate();
+        authenticate('username', 'password', function(err, result) {
+            if (err) {
+                console.log(`There was an error authenticating: ${err}`);
+                return next(err);
+            }
+
+            console.log('authentication successful ...?');
+            res.redirect('/profile.html');
+        });
         // passport.authenticate('local', function(req, res, next) {
         //     console.log('authenticating');
         //     res.redirect('/profile.html');
         // });
     });
-}, passport.authenticate('local', { failureRedirect: '/signup.html'}),
-    function(req, res, next) {
-        res.redirect('/profile.html');
-    } 
-);
+});
 
-
-    router.post('/signup', function(req, res, next) {
-        // console.log(`attempting signup ${req.body.username}`);
-        myDataBase.findOne({ username: req.body.username}, function(err, user) {
-            if (err) { next(err); }
-            else if (user) { res.redirect('/profile.html'); }
-            else {
-                const hash = bcrypt.hashSync(req.body.password, 12);
-                myDataBase.insertOne ({
-                    username: req.body.username,
-                    password: hash
-                }, function(err, doc) {
-                        if (err) { return next(err); }
-                        else { res.redirect('/profile.html'); }
-                    }
-                )
-            }
-        })
-    },  passport.authenticate('local', { failureRedirect: '/signup.html'}),
-            function(req, res, next) {
-                res.redirect('/profile.html');
-            }
-    );
+router.use('/profile.html', ensureAuthenticated);
 
 router.post('/login/password', passport.authenticate('local', {
     successRedirect: '/profile.html',
