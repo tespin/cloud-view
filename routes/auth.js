fc17a81b0d87031d354c1308cd2344671e807695
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-27 19:20:25 -0700

    test

--- routes/auth.js
+++ routes/auth.js
@@ -9,8 +9,8 @@ const app = require('../app');
 const ObjectID = require('mongodb').ObjectId;
 const router = express.Router();
 
-passport.use(User.createStrategy());
-// passport.use(new LocalStrategy(User.authenticate()));
+// passport.use(User.createStrategy());
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
