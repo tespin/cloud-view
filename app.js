3ce03527e869fd68f7fe5f7ef92c08d648c55a83
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-27 22:14:30 -0700

    adding error handle to store

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 // // const SQLiteStore = require('connect-sqlite3')(session);
 const authRouter = require('./routes/auth');
+let store = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    databaseName: 'database',
+    collection: 'sessions'
+}, function(error) {
+    console.log(`error with store: ${error}`)
+});
+
+store.on('error', function(error) {
+    console.log(`store on error: ${error}`);
+})
+
 const app = express();
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({ extended: true }));
-const port = process.env.PORT || 80;
 
+const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`listening at port ${port}`));
+
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
