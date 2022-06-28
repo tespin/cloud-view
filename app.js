24a29b1e666a3e906797dfcd9874b8f160c61d35
Tristan Espinoza <tristan@Tristans-MBP.lan>
2022-06-28 00:54:27 -0700

    attempting to get logged in user

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post('/api', (request, response) => {
 
 app.post('/save', (request, response) => {
     const data = request.body;
-    console.log(`request made by ${req.user}`);
+    console.log(`request made by ${request.user}`);
     response.json({
         status: 'success',
         base64: data.base64
