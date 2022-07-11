829c15461d35daee1ae21d2a9ba7ea862ab3c46a
tespin <tristan.m.espinoza@gmail.com>
2022-07-11 10:27:07 -0700

    testing save button

--- public/scripts/cloudControl.js
+++ public/scripts/cloudControl.js
@@ -10,6 +10,20 @@ window.addEventListener('load', async (event) => {
 
 document.getElementById('save').addEventListener('click', async (event) => {
     console.log('save button clicked');
+
+    let img = document.getElementById("result");
+
+    if (img.getAttribute('src') === '') {
+        let errorBox = document.getElementById('errorBox');
+        errorBox.style.display = 'block';
+        errorBox.innerText = "Please obtain a valid image before saving.";
+
+        return console.log("Please obtain a valid image before saving.");
+    }
+    
+    // getBase64Image(img, postBase64);
+    let savedImage = await saveImage(img);
+    addImages(savedImage, indxObject);
 })
 
 async function fetchImages() {
@@ -56,4 +70,31 @@ function addImages(imgs, obj) {
         gallery.append(entryDiv);
         obj.index++;
     })
+}
+
+async function saveImage(image) {
+    
+    let errorBox = document.getElementById("errorBox");
+    errorBox.style.display = "none";
+
+    let canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0);
+    let base64 = canvas.toDataURL("image/png");
+
+    const data = { base64 };
+    const options = {
+        method: 'POST', headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    const response = await fetch('save', options);
+    const json = await response.json();
+
+    return [base64];
 }
