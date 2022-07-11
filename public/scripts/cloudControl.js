0e393d85735684526daef2b2a89eb8ec233c2d07
tespin <tristan.m.espinoza@gmail.com>
2022-07-11 10:32:05 -0700

    adding event listener to each image

--- public/scripts/cloudControl.js
+++ public/scripts/cloudControl.js
@@ -1,4 +1,5 @@
 let indxObject = { index: 0 };
+let selected = [];
 
 window.addEventListener('load', async (event) => {
     console.log('page loaded');
@@ -57,21 +58,6 @@ async function fetchImages() {
     // });
 }
 
-function addImages(imgs, obj) {
-    let gallery = document.getElementById('gallery');
-    imgs.forEach(element => {
-        let entryDiv = document.createElement('div');
-        entryDiv.className = 'entry';
-
-        let img = new Image();
-        img.src = element;
-        img.dataset.index = obj.index;
-        entryDiv.append(img);
-        gallery.append(entryDiv);
-        obj.index++;
-    })
-}
-
 async function saveImage(image) {
     
     let errorBox = document.getElementById("errorBox");
@@ -98,3 +84,62 @@ async function saveImage(image) {
 
     return [base64];
 }
+
+function addImages(imgs, obj) {
+    let gallery = document.getElementById('gallery');
+    imgs.forEach(element => {
+        let entryDiv = document.createElement('div');
+        entryDiv.className = 'entry';
+
+        let img = new Image();
+        img.src = element;
+        img.dataset.index = obj.index;
+
+        img.addEventListener('click', event => {
+            let current = event.target;
+            if (current.style.border == "") {
+                current.style.border = "3px solid #005180";
+                selected.push(current.dataset.index);
+                console.log(`Image ${selected.dataset.index} selected`)
+                console.log(`Images to be deleted: ${toBeDeleted}`);
+            } else {
+                selected.style.border = "";
+                let idx = toBeDeleted.indexOf(selected.dataset.index);
+                if (idx > -1) { toBeDeleted.splice(idx, 1); }
+                console.log(`Image ${selected.dataset.index} deselected`)
+                console.log(`Images to be deleted: ${toBeDeleted}`);
+            }
+        })
+
+        entryDiv.append(img);
+        gallery.append(entryDiv);
+        obj.index++;
+    })
+}
+
+// function selectImages() {
+//     let imgs = document.querySelectorAll("div#gallery img");
+//     let toBeDeleted = [];
+//     imgs.forEach(img => {
+//         // img.remove();
+//         // let showBorder = false;
+//         img.addEventListener('click', event => {
+//             // showBorder != showBorder;
+//             // console.log(showBorder);
+//             // console.log(savedImages.length);
+//             let selected = event.target;
+//             if (selected.style.border == "") {
+//                 selected.style.border = "3px solid #005180";
+//                 toBeDeleted.push(selected.dataset.index);
+//                 console.log(`Image ${selected.dataset.index} selected`)
+//                 console.log(`Images to be deleted: ${toBeDeleted}`);
+//             } else {
+//                 selected.style.border = "";
+//                 let idx = toBeDeleted.indexOf(selected.dataset.index);
+//                 if (idx > -1) { toBeDeleted.splice(idx, 1); }
+//                 console.log(`Image ${selected.dataset.index} deselected`)
+//                 console.log(`Images to be deleted: ${toBeDeleted}`);
+//             }
+//         });
+//     });
+// }
