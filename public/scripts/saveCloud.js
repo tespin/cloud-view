4617632638a0adb9e314d53161c8c94ba2a6401a
tespin <tristan.m.espinoza@gmail.com>
2022-07-10 18:54:08 -0700

    changing save cloud function

--- public/scripts/saveCloud.js
+++ public/scripts/saveCloud.js
@@ -11,47 +11,22 @@ document.getElementById('save').addEventListener('click', event => {
         return console.log("Please obtain a valid image before saving.");
     }
     
-    getBase64Image(img, postBase64);
-
-    // let base64 = getBase64Image(img, postBase64);
-    // console.log(img.src);
-    // console.log(getBase64Image(img));
-    // let base64 = getBase64Image(async img => {
-    //     console.log('retrieving');
-    // });
-    // let base64 = getBase64Image(async img => {
-    //     const data = { base64 };
-
-    //     const options = {
-    //         method: 'POST', headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(data)
-    //     };
-
-    //     const response = await fetch('save', options);
-    //     const json = await response.json();
-    //     console.log(json.base64);
-    // });
+    // getBase64Image(img, postBase64);
+    saveImage(img);
 });
 
-function getBase64Image(img, cb) {
-    let errorBox = document.getElementById('errorBox');
+async function saveImage(image) {
+    let errorBox = document.getElementById("errorBox");
     errorBox.style.display = "none";
 
     let canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
+    canvas.width = image.width;
+    canvas.height = image.height;
+
     let ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    let dataURL = canvas.toDataURL("image/png");
-    // return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-    cb(dataURL);
-  }
-  
-async function postBase64(base64) {
-    // console.log('posting:');
-    // console.log(dateUrl);
+    ctx.drawImage(image, 0, 0);
+    let base64 = canvas.toDataURL("image/png");
+
     const data = { base64 };
     const options = {
         method: 'POST', headers: {
@@ -75,20 +50,46 @@ async function postBase64(base64) {
 
     entryDiv.append(img);
     gallery.append(entryDiv);
-    // let savedImages = json.user.saved;
-    
-    
-
-    // let gallery = document.getElementById('gallery');
-
-    // savedImages.forEach(element => {
-    //     let entryDiv = document.createElement('div');
-    //     entryDiv.className = 'entry';
-
-    //     let img = new Image();
-    //     img.src = element;
-
-    //     entryDiv.append(img);
-    //     gallery.append(entryDiv);
-    // });
 }
+
+// function getBase64Image(img, cb) {
+//     let errorBox = document.getElementById('errorBox');
+//     errorBox.style.display = "none";
+
+//     let canvas = document.createElement("canvas");
+//     canvas.width = img.width;
+//     canvas.height = img.height;
+//     let ctx = canvas.getContext("2d");
+//     ctx.drawImage(img, 0, 0);
+//     let dataURL = canvas.toDataURL("image/png");
+//     // return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+//     cb(dataURL);
+//   }
+  
+// async function postBase64(base64) {
+//     // console.log('posting:');
+//     // console.log(dateUrl);
+//     const data = { base64 };
+//     const options = {
+//         method: 'POST', headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     };
+
+//     const response = await fetch('save', options);
+//     const json = await response.json();
+//     let images = json.user.saved;
+//     // let size = images.length;
+//     // console.log(size);
+//     let gallery = document.getElementById('gallery');
+
+//     let entryDiv = document.createElement('div');
+//     entryDiv.className = 'entry';
+
+//     let img = new Image();
+//     img.src = images.at(-1);
+
+//     entryDiv.append(img);
+//     gallery.append(entryDiv);
+// }
