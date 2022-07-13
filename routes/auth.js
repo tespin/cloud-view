eaac6fc8f16f564d4a1b63c209f3767142268913
tespin <tristan.m.espinoza@gmail.com>
2022-07-12 21:04:06 -0700

    separated lines:

--- routes/auth.js
+++ routes/auth.js
@@ -128,8 +128,8 @@ router.post('/user', (req, res, done) => {
 router.post('/cotd', (req, res, done) => {
     User.find({},
         // { saved: 1}, // works, returns every document 
-        // {saved : 1},
-        { saved: { $exists: true, $not: { $size: 1 }}},
+        {saved : 1},
+        { $exists: true, $not: { $size: 1 }},
         // { $elemMatch: { saved: { $exists: true, $not: { $size: 1}}}},
         // { saved: { "saved.0": { "$exists": true}}},
         // { "$match": { "saved.0": { "$exists": true }}},
