8b4802a627d224c28aabeda9348523f764d3b703
tespin <tristan.m.espinoza@gmail.com>
2022-07-12 18:37:05 -0700

    match exists is true

--- routes/auth.js
+++ routes/auth.js
@@ -132,7 +132,8 @@ router.post('/cotd', (req, res, done) => {
         // { saved: {$elemMatch: }}
         // { $match: { saved: {$elemMatch: { $exists:true }}}},
         // { saved: { $elemMatch: { $exists: true}}},
-        { saved: { $elemMatch: { $not: { $size: 0 }}}},
+        // { saved: { $elemMatch: { $not: { $size: 0 }}}},
+        { saved: { $match: { $exists: true }}},
         (err, result) => {
             if (err) return console.log(err);
             res.json({
