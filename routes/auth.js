efe89aea5f6bb6c2292a4137d0d8cd746bdf453e
tespin <tristan.m.espinoza@gmail.com>
2022-07-12 22:22:13 -0700

    expr gt dize saved 0

--- routes/auth.js
+++ routes/auth.js
@@ -127,7 +127,8 @@ router.post('/user', (req, res, done) => {
 
 router.post('/cotd', (req, res, done) => {
     User.find({},
-        { saved: 1}, // works, returns every document 
+        { $expr: { $gt: [{ $size: saved}, 0]}},
+        //{ saved: 1}, // works, returns every document 
         // {saved: { $size: 1}},
         // {"req.user.saved": 1},
         // { $size: { saved: { $not: 0}}},
