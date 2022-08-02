7df49a86836a8c5e23f46ac69a62f019581b64c1
tespin <tristan.m.espinoza@gmail.com>
2022-08-02 16:36:57 -0700

    signup takes you to correct page

--- routes/auth.js
+++ routes/auth.js
@@ -44,7 +44,7 @@ router.post('/signup', function(req, res, done) {
         }
         
         passport.authenticate('local') (req, res, function() {
-            res.redirect('/profile.html');
+            res.redirect('/home.html');
         });
     });
 });
