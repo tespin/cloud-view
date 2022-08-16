545c5a6bbebff5e65bdc139d302afd46610ea623
tespin <tristan.m.espinoza@gmail.com>
2022-08-16 14:47:24 -0700

    adding button changes

--- public/scripts/toggleMenu.js
+++ public/scripts/toggleMenu.js
@@ -1,6 +1,18 @@
 const toggle_btn = document.querySelector('#menu-toggle');
-const menu = document.querySelector('nav');
+const close_btn = document.getElementById('close-menu');
+const menu = document.querySelector('div.menu');
 toggle_btn.addEventListener('click', event => {
-    toggle_btn.classList.toggle('button-open');
+    toggle_btn.classList.toggle('menu-open');
     menu.classList.toggle('active');
+
+    if (toggle_btn.classList.contains('menu-open')) {
+        toggle_btn.innerHTML = 'x';
+        toggle_btn.setAttribute('aria-expanded', true);
+        toggle_btn.setAttribute('aria-label', 'Close menu');
+
+    } else {
+        toggle_btn.innerHTML = 'Menu';
+        toggle_btn.setAttribute('aria-expanded', false);
+        toggle_btn.setAttribute('aria-label', 'Open menu');
+    }
 })
