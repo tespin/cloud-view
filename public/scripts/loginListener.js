10fd2eb836686a8ffef3891f0b9d2c2e486217ce
tespin <tristan.m.espinoza@gmail.com>
2022-08-26 12:24:26 -0700

    testing login listener:

---
+++ public/scripts/loginListener.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('submit');
+form.addEventListener('submit', event => {
+    const data = new FormData(form);
+    const options = {
+        method: 'POST', headers: {
+            'Content-Type': 'application/jason'
+        },
+        body: JSON.stringify(data)
+    };
+    const response = await fetch('delete', options);
+    const json = await response.json();
+    console.log(json);
+})
+
+// const data = {selected};
+//     const options = {
+//         method: 'POST', headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     };
+//     const response = await fetch('delete', options);
+//     const json = await response.json();
+
+//     window.location = json.redirect;
