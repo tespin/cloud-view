952217cb3259dc5427bfcba6a93cd4f0b2d6ed5a
tespin <tristan.m.espinoza@gmail.com>
2022-09-07 16:59:34 -0700

    adding login listener

--- public/scripts/signupLoginListener.js
+++ public/scripts/signupLoginListener.js
@@ -7,6 +7,7 @@
 const loginForm = document.getElementById('login');
 loginForm.addEventListener('submit', event =>  {
     fetchErrors();
+    event.preventDefault();
 })
 
 async function fetchErrors() {
