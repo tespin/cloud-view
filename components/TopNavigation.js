5d07d557fd5e9811092f8ceeb9a891f9fc5b594d
tespin <tristan.m.espinoza@gmail.com>
2023-03-09 06:27:25 -0800

    adding radix ui nav menu

--- components/TopNavigation.js
+++ components/TopNavigation.js
@@ -1,15 +1,37 @@
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { CaretUpIcon, CaretDownIcon } from '@radix-ui/react-icons';
+
 const TopNavigation = (props) => {
     return (
         <>
             <div className='xs:flex xs:flex-row'>
                 <a href='#' aria-label='cloud view homepage'>Cloud View</a>
-                <nav aria-label='main navigation'>
+                <NavigationMenu.Root>
+                    <NavigationMenu.List>
+                        <NavigationMenu.Item>
+                            <NavigationMenu.Trigger>
+                                About <CaretDownIcon aria-hidden/>
+                            </NavigationMenu.Trigger>
+                        </NavigationMenu.Item>
+                        <NavigationMenu.Item>
+                            <NavigationMenu.Trigger>
+                                Learn <CaretDownIcon aria-hidden/>
+                            </NavigationMenu.Trigger>
+                        </NavigationMenu.Item>
+                        <NavigationMenu.Item>
+                            <NavigationMenu.Trigger>
+                                Storage
+                            </NavigationMenu.Trigger>
+                        </NavigationMenu.Item>
+                    </NavigationMenu.List>
+                </NavigationMenu.Root>
+                {/* <nav aria-label='main navigation'>
                     <ul>
                         <li>About</li>
                         <li>Privacy</li>
                         <li>Storage</li>
                     </ul>
-                </nav>
+                </nav> */}
             </div>
         </>
     );
