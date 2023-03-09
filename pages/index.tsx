4218478feca7153be3307f9501c34feab525f8d6
tespin <tristan.m.espinoza@gmail.com>
2023-03-09 05:56:48 -0800

    migrating next/font to built in instead of dep

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Inter } from "next/font/google"
 
 import Container from '@/components/UI/Container';
 import TopNavigation from '@/components/TopNavigation';
