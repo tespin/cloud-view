e6bead1e870afb6e6aefccfffdfc7fd0115532bc
tespin <tristan.m.espinoza@gmail.com>
2023-03-20 14:34:07 -0700

    cleanup - margins

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 
 import Container from '@/components/UI/Container';
 import MainNav from '@/components/MainNav';
+import Placeholder from '@/components/UI/Placeholder';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,11 +18,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container className='xs:flex-col xs:justify-center xs:items-center xs:w-full'>
-        <Container className='xs:flex-col xs:justify-center xs:items-center xs:max-w-md xs:m-4'>
+        <Container className='xs:flex-col xs:justify-center xs:items-center xs:max-w-md xs:my-4 xs:mx-8'>
           <MainNav/>
-            <main>
-                <h1 className='xs:mt-12 xs:text-center xs:text-4xl'>Your personal cloud storage solution.</h1>
-            </main>
+          <main>
+              <h1 className='xs:mt-12 xs:text-center xs:text-4xl'>Your personal cloud storage solution.</h1>
+          </main>
+          <Placeholder className='xs:mt-8' />
         </Container>
       </Container>
     </>
