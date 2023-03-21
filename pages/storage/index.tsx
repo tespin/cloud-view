011066047c8f2948c0883aea2b2aba73a01c23f9
tespin <tristan.m.espinoza@gmail.com>
2023-03-20 18:38:17 -0700

    styling added to pages

--- pages/storage/index.tsx
+++ pages/storage/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
+import Container from '@/components/UI/Container';
+import MainNav from '@/components/MainNav';
 
 const StoragePage = () => {
+    const [authenticated, setAuthenticated] = useState(false);
+
     return (
         <>
-            <h2>Storage</h2>
+            <Container className='xs:flex-col xs:justify-center xs:items-center xs:w-full'>
+                <Container className='xs:flex-col xs:justify-center xs:items-center xs:max-w-sm xs:my-4 xs:mx-8'>
+                    <MainNav/>
+                    <main>
+                        <h2 className='xs:text-3xl xs:mt-9'>Storage</h2>
+                        { authenticated 
+                            ? <>
+                                <p>Logged in</p>
+                            </>
+                            : <> <div className='xs:flex xs:flex-col xs:space-y-4 xs:mt-4 xs:text-base-md'>
+                                    <p>Log in to view your cloud storage. You can edit or download from your storage once you’ve logged in.</p>
+                                </div>
+                                <button className='xs:w-full xs:mt-9 xs:border-2 xs:border-transparent xs:bg-base xs:hover:bg-base-darkMd xs:text-white xs:py-3 xs:rounded-md'>Log in</button>
+                            </> 
+                        }
+                        
+                    </main>
+                </Container>
+            </Container>
         </>
-    );
+    )
 }
 
 export default StoragePage;
