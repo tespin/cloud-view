02cf11679d76567d52e3bb131fea0fba414c9ffd
tespin <tristan.m.espinoza@gmail.com>
2023-03-20 18:37:27 -0700

    custom focus on close button when dialog is open

--- components/UI/Dialog.tsx
+++ components/UI/Dialog.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
 import { HamburgerMenuIcon, CaretUpIcon, CaretDownIcon, Cross2Icon } from '@radix-ui/react-icons';
 
 const Dialog = (props) => {
     const [open, setOpen] = useState(false);
+    const dialogCloseButton = useRef(null);
 
     return (
         <>
@@ -16,13 +17,13 @@ const Dialog = (props) => {
                 </DialogPrimitive.Trigger>
                 <DialogPrimitive.Portal>
                     <DialogPrimitive.Overlay className={`xs:fixed xs:inset-0 backdrop-blur-sm xs:bg-base/20 ${open ? 'animate-overlayShow' : 'animate-overlayHide'}`}/>
-                    <DialogPrimitive.Content className={`xs:fixed xs:bg-white xs:rounded-lg top-4 right-4 p-4 w-full max-w-xs ${open ? 'animate-contentShow' : 'animate-contentHide'}`}>
+                    <DialogPrimitive.Content onOpenAutoFocus={(event) => {
+                        dialogCloseButton.current?.focus();
+                        event.preventDefault();
+                    }} className={`xs:fixed xs:bg-white xs:rounded-lg top-4 right-4 p-4 w-full max-w-xs ${open ? 'animate-contentShow' : 'animate-contentHide'}`}>
                         {props.children}
-                        <DialogPrimitive.Close asChild>
-                            <button onClick={(event) => {
-                                setOpen(false);
-                                event.preventDefault();
-                            }} className='flex justify-center items-center xs:hover:text-base-semiMd absolute w-6 h-6 top-4 right-4'>
+                        <DialogPrimitive.Close ref={dialogCloseButton} asChild>
+                            <button className='flex justify-center items-center xs:hover:text-base-semiMd absolute w-6 h-6 top-4 right-4'>
                                 <Cross2Icon />
                             </button>
                         </DialogPrimitive.Close>
