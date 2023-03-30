b04fd40e1af547de8e5954cc093e51b3f4600140
tespin <tristan.m.espinoza@gmail.com>
2023-03-29 23:25:35 -0700

    extending tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-const { DefaultRouteMatcherManager } = require('next/dist/server/future/route-matcher-managers/default-route-matcher-manager');
+const {
+  DefaultRouteMatcherManager,
+} = require('next/dist/server/future/route-matcher-managers/default-route-matcher-manager');
 
 /** @type {import('tailwindcss').Config} */
 
@@ -6,13 +8,13 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     screens: {
-      'xs': '320px',
-      ...defaultTheme.screens
+      xs: '320px',
+      ...defaultTheme.screens,
     },
     extend: {
       colors: {
@@ -22,47 +24,52 @@ module.exports = {
           semiMd: '#A9ADB5',
           light: '#D9D8DE',
           spLight: '#F0F2F5',
-          DEFAULT: '#101622'
-        }
+          DEFAULT: '#101622',
+        },
+        errorRed: {
+          DEFAULT: '#E8361E',
+        },
       },
       keyframes: {
         fadeIn: {
-          'from': { opacity: '0'},
-          'to': { opacity: '1'}
+          from: { opacity: '0' },
+          to: { opacity: '1' },
         },
         fadeOut: {
-          'from': { opacity: '1'},
-          'to': { opacity: '0' } 
+          from: { opacity: '1' },
+          to: { opacity: '0' },
         },
         growSm: {
-          'from': { transform: 'scale(0.96)' },
-          'to': { transform: 'scale(1)'}
+          from: { transform: 'scale(0.96)' },
+          to: { transform: 'scale(1)' },
         },
         shrinkSm: {
-          'from': { transform: 'scale(1)'},
-          'to': { transform: 'scale(0.96)'}
+          from: { transform: 'scale(1)' },
+          to: { transform: 'scale(0.96)' },
         },
         topDown: {
-          'from': { transform: 'translateY(-1rem)'},
-          'to': { transform: 'translateY(0)'}
+          from: { transform: 'translateY(-1rem)' },
+          to: { transform: 'translateY(0)' },
         },
         downTop: {
-          'from': { transform: 'translateY(0)'},
-          'to': { transform: 'translateY(-1rem)'}
+          from: { transform: 'translateY(0)' },
+          to: { transform: 'translateY(-1rem)' },
         },
         aboutHeight: {
-          'from': { maxHeight: '0'},
-          'to': { maxHeight: '64rem'}
-        }
+          from: { maxHeight: '0' },
+          to: { maxHeight: '64rem' },
+        },
       },
       animation: {
-        'contentShow': 'fadeIn 0.15s ease-in, growSm 0.15s ease-in, topDown 0.15s ease-in',
-        'contentHide': 'fadeOut 0.15s ease-in, shrinkSm 0.15s ease-in, downTop 0.15s ease-in',
-        'overlayShow': 'fadeIn 0.12s ease-in',
-        'overlayHide': 'fadeOut 0.12s ease-in',
-        'aboutShow': 'aboutHeight 0.12s ease-in'
-      }
+        contentShow:
+          'fadeIn 0.15s ease-in, growSm 0.15s ease-in, topDown 0.15s ease-in',
+        contentHide:
+          'fadeOut 0.15s ease-in, shrinkSm 0.15s ease-in, downTop 0.15s ease-in',
+        overlayShow: 'fadeIn 0.12s ease-in',
+        overlayHide: 'fadeOut 0.12s ease-in',
+        aboutShow: 'aboutHeight 0.12s ease-in',
+      },
     },
   },
   plugins: [],
-}
+};
