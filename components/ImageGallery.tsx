f629fcdb3e49cbc0d464a3e3df4a82934dd58fa5
tespin <tristan.m.espinoza@gmail.com>
2023-04-05 13:35:37 -0700

    placeholder component is now a more specific container

---
+++ components/ImageGallery.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImageGallery = () => {
+  return <></>;
+};
+
+export default ImageGallery;
--- components/Placeholder.tsx
+++ components/Placeholder.tsx
@@ -1,44 +1,19 @@
 import React, { useState } from 'react';
-import Image from 'next/image';
 import Container from './UI/Container';
-import cloudImg from '@/public/cloud_3542.png';
 
 interface PlaceholderProps {
   className: string;
+  children?: React.ReactNode;
 }
 
-const Placeholder = ({ className }: PlaceholderProps) => {
-  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
-  const [cloudImage, setCloudImage] = useState<string | null>(null);
-
-  const imageHandler = () => {
-    setImageLoaded(true);
-  };
-
+const Placeholder = ({ className, children }: PlaceholderProps) => {
   return (
     <>
-      {imageLoaded ? (
-        <div className={`${className}`}>
-          <Image src={cloudImg} alt='cloud photo' />
-          <button
-            className=' xs:w-full xs:mt-4 xs:bg-base xs:hover:bg-base-darkMd xs:focus:outline-none xs:focus:ring xs:text-white xs:py-3 xs:px-6 xs:rounded-md'
-            onClick={imageHandler}
-          >
-            Request a new cloud
-          </button>
-        </div>
-      ) : (
-        <Container
-          className={`xs:justify-center xs:items-center xs:w-96 xs:h-96 xs:rounded-md xs:border-dashed xs:border-black xs:border-[1px] ${className}`}
-        >
-          <button
-            className='xs:bg-base xs:hover:bg-base-darkMd xs:focus:outline-none xs:focus:ring xs:text-white xs:py-3 xs:px-6 xs:rounded-md'
-            onClick={imageHandler}
-          >
-            Request a cloud
-          </button>
-        </Container>
-      )}
+      <Container
+        className={`xs:justify-center xs:items-center xs:w-96 xs:h-96 xs:rounded-md xs:border-dashed xs:border-black xs:border-[1px] ${className}`}
+      >
+        {children}
+      </Container>
     </>
   );
 };
