092d25a763650792e7213b525bb7adcfcaa0ab46
tespin <tristan.m.espinoza@gmail.com>
2023-04-05 13:34:58 -0700

    modifying placeholder logic for loaded images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import React, { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-
+import cloudImg from '@/public/cloud_3542.png';
 import Container from '@/components/UI/Container';
 import MainNav from '@/components/MainNav';
 import Placeholder from '@/components/Placeholder';
@@ -9,6 +10,13 @@ import Placeholder from '@/components/Placeholder';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+  const [cloudImage, setCloudImage] = useState<string | null>(null);
+
+  const imageHandler = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <>
       <Head>
@@ -25,7 +33,26 @@ export default function Home() {
               Your personal cloud storage solution.
             </h1>
           </main>
-          <Placeholder className='xs:mt-8' />
+          {imageLoaded ? (
+            <div className={`xs:mt-8`}>
+              <Image src={cloudImg} alt='cloud photo' />
+              <button
+                className=' xs:w-full xs:mt-4 xs:bg-base xs:hover:bg-base-darkMd xs:focus:outline-none xs:focus:ring xs:text-white xs:py-3 xs:px-6 xs:rounded-md'
+                onClick={imageHandler}
+              >
+                Request a new cloud
+              </button>
+            </div>
+          ) : (
+            <Placeholder className='xs:mt-8'>
+              <button
+                className='xs:bg-base xs:hover:bg-base-darkMd xs:focus:outline-none xs:focus:ring xs:text-white xs:py-3 xs:px-6 xs:rounded-md'
+                onClick={imageHandler}
+              >
+                Request a cloud
+              </button>
+            </Placeholder>
+          )}
         </Container>
       </Container>
     </>
