68fe32ce8f9c87ad7b2ddbe97d4f35ef72172a8f
tespin <tristan.m.espinoza@gmail.com>
2023-04-14 21:41:44 -0700

    lightboxes for each image

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -3,17 +3,18 @@ import Image from 'next/image';
 import ImageModal from './ImageModal';
 
 interface GalleryProps {
-  links: { src: string; id: string }[];
+  links: { src: string; id: string; date: string }[];
 }
 
 const ImageGallery = ({ links }: GalleryProps) => {
   return (
     <>
       <div className='xs:grid xs:grid-cols-2 xs:gap-8 xs:mt-8'>
-        {links.map((link) => {
-          // return <p key={link.id}>{link.url}</p>;
-          return <ImageModal link={link} key={link.id}></ImageModal>;
-        })}
+        {links[0].src.length > 0 &&
+          links.map((link) => {
+            // return <p key={link.id}>{link.url}</p>;
+            return <ImageModal link={link} key={link.id} />;
+          })}
       </div>
     </>
   );
--- components/ImageModal.tsx
+++ components/ImageModal.tsx
@@ -1,39 +1,51 @@
 import React from 'react';
 import Image from 'next/image';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
 
 interface ImageModalProps {
-  link: { src: string; id: string };
+  link: { src: string; id: string; date: string };
 }
 
 const ImageModal = ({ link }: ImageModalProps) => {
   return (
-    <DialogPrimitive.Root>
-      <DialogPrimitive.Trigger asChild>
-        <div className='xs:relative xs:w-full xs:h-48'>
-          <Image
-            src={link.src}
-            alt='cloud photo'
-            fill
-            className='xs:rounded-lg'
+    <>
+      <DialogPrimitive.Root>
+        <DialogPrimitive.Trigger asChild>
+          <button className='xs:focus:outline-none xs:focus:ring xs:rounded-lg'>
+            <Image
+              src={link.src}
+              alt='View image'
+              width={0}
+              height={0}
+              className='xs:rounded-lg xs:w-full xs:h-auto'
+            />
+          </button>
+        </DialogPrimitive.Trigger>
+        <DialogPrimitive.Portal>
+          <DialogPrimitive.Overlay
+            className={`xs:fixed xs:inset-0 backdrop-blur-sm xs:bg-base/20`}
           />
-        </div>
-      </DialogPrimitive.Trigger>
-      <DialogPrimitive.Portal>
-        <DialogPrimitive.Overlay
-          className={`xs:fixed xs:inset-0 backdrop-blur-sm xs:bg-base/20`}
-        />
-        <DialogPrimitive.Content className='xs:fixed xs:inset-0 xs:mx-auto xs:flex xs:justify-center xs:items-center xs:max-w-xs'>
-          <Image
-            src={link.src}
-            alt='cloud photo'
-            width={300}
-            height={300}
-            className='xs:rounded-lg'
-          />
-        </DialogPrimitive.Content>
-      </DialogPrimitive.Portal>
-    </DialogPrimitive.Root>
+          <DialogPrimitive.Content className='xs:fixed xs:inset-0 xs:mx-auto xs:flex xs:flex-col xs:justify-center xs:items-center xs:w-full xs:h-auto xs:max-w-xs'>
+            <div className='xs:flex xs:flex-row xs:w-full xs:justify-between'>
+              <DialogPrimitive.Close asChild>
+                <button className='xs:focus:outline-none xs:focus:ring xs:hover:text-base-semiMd xs:text-white'>
+                  <ArrowLeftIcon className='xs:w-6 xs:h-6' />
+                </button>
+              </DialogPrimitive.Close>
+              <p className='xs:text-white'>February 20, 2023</p>
+            </div>
+            <Image
+              src={link.src}
+              alt='cloud photo'
+              width={0}
+              height={0}
+              className='xs:mt-1 xs:rounded-lg xs:w-full xs:h-auto'
+            />
+          </DialogPrimitive.Content>
+        </DialogPrimitive.Portal>
+      </DialogPrimitive.Root>
+    </>
   );
 };
 
